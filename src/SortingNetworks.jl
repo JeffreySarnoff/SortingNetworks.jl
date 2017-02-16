2e526e62e28084fabe0db5a3ca3b16f35c829677
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-02-16 09:30:26 -0500

    Update and rename TinySorts.jl to SortingNetworks.jl

---
+++ src/SortingNetworks.jl
@@ -0,0 +1,708 @@
+"""
+Sorts 1,2,..,16 values using the best known sorting network for each.
+
+Accepts values of any one type T that supports `(<)(a::T, b::T)`
+
+Returns a tuple containing the sorted values
+
+>>    sort(a::T), sort(a::T, b::T), ..., sort(a::T, b::T,.., p::T)
+
+    sort( 8, 5, 9, 11, 2 ) == ( 2, 5, 8, 9, 11 )
+
+"""
+module SortingNetworks
+
+
+import Base: sort
+
+
+#=
+    The sixteen sort methods use sorting networks of least work.
+    Sorting networks that sort 1,2,.. 16 values are provided.
+
+
+    sort(x1::T, .., xn::T)::NTuple{n,T}
+    sort(tup::NTuple{n,T})::NTuple{n,T}
+
+
+    sort(x1::T), sort(x1::T, x2::T), .., sort(x1::T, .., x16::T)
+
+    sort(tup::NTuple{1,T}), .., sort(tup::NTuple{16,T})
+=#
+
+const NARGS_MIN = 1
+const NARGS_MAX = 16
+
+
+
+
+#    sort 1 value with 0 minmaxs in 1 stage
+
+function sort(a::T) where T
+
+    return (a,)
+end
+
+#    sort 2 values with 1 minmaxs in 1 stage
+
+function sort(a::T, b::T) where T
+
+    a, b = minmax(a, b)
+
+    return a,b
+end
+
+#    sort 3 values with 3 minmaxs in 3 parallel stages
+
+function sort(a::T, b::T, c::T) where T
+
+    b, c = minmax(b, c)
+
+    a, c = minmax(a, c)
+
+    a, b = minmax(a, b)
+
+    return a, b, c
+end
+
+#    sort 4 values with 5 minmaxs in 3 parallel stages
+
+function sort(a::T, b::T, c::T, d::T) where T
+
+    a, b = minmax(a, b)
+    c, d = minmax(c, d)
+
+    a, c = minmax(a, c)
+    b, d = minmax(b, d)
+
+    b, c = minmax(b, c)
+
+    return a, b, c, d
+end
+
+#    sort 5 values with 9 minmaxs in 5 parallel stages
+
+  function sort(a::T, b::T, c::T, d::T, e::T) where T
+
+    a, e = minmax(a, e)
+    b, d = minmax(b, d)
+
+    a, c = minmax(a, c)
+
+    c, e = minmax(c, e)
+    a, b = minmax(a, b)
+
+    b, c = minmax(b, c)
+    d, e = minmax(d, e)
+
+    return a, b, c, d, e
+end
+
+#    sort 6 values with 12 minmaxs in 6 parallel stages
+
+function sort(a::T, b::T, c::T, d::T, e::T,
+              f::T) where T
+
+    b, c = minmax(b, c)
+    e, f = minmax(e, f)
+
+    a, c = minmax(a, c)
+    d, f = minmax(d, f)
+
+    a, b = minmax(a, b)
+    d, e = minmax(d, e)
+    c, f = minmax(c, f)
+
+    a, d = minmax(a, d)
+    b, e = minmax(b, e)
+
+    c, e = minmax(c, e)
+    b, d = minmax(b, d)
+
+    c, d = minmax(c, d)
+
+    return a, b, c, d, e, f
+end
+
+#    sort 7 values with 16 minmaxs in 6 parallel stages
+
+function sort(a::T, b::T, c::T, d::T, e::T,
+              f::T, g::T) where T
+
+    a, e = minmax(a, e)
+    b, f = minmax(b, f)
+    c, g = minmax(c, g)
+
+    a, c = minmax(a, c)
+    b, d = minmax(b, d)
+    e, g = minmax(e, g)
+
+    c, e = minmax(c, e)
+    d, f = minmax(d, f)
+    a, b = minmax(a, b)
+
+    c, d = minmax(c, d)
+    e, f = minmax(e, f)
+
+    b, e = minmax(b, e)
+    d, g = minmax(d, g)
+
+    b, c = minmax(b, c)
+    d, e = minmax(d, e)
+    f, g = minmax(f, g)
+
+    return a, b, c, d, e, f, g
+end
+
+#    sort 8 values with 19 minmaxs in 7 parallel stages
+
+function sort(a::T, b::T, c::T, d::T, e::T,
+              f::T, g::T, h::T) where T
+
+    a, b = minmax(a, b)
+    c, d = minmax(c, d)
+    e, f = minmax(e, f)
+    g, h = minmax(g, h)
+
+    a, c = minmax(a, c)
+    b, d = minmax(b, d)
+    e, g = minmax(e, g)
+    f, h = minmax(f, h)
+
+    b, c = minmax(b, c)
+    f, g = minmax(f, g)
+    a, e = minmax(a, e)
+    d, h = minmax(d, h)
+
+    b, f = minmax(b, f)
+    c, g = minmax(c, g)
+
+    b, e = minmax(b, e)
+    d, g = minmax(d, g)
+
+    c, e = minmax(c, e)
+    d, f = minmax(d, f)
+
+    d, e = minmax(d, e)
+
+    return a, b, c, d, e, f, g, h
+end
+
+#    sort 9 values with 25 minmaxs in 9 parallel stages
+
+function sort(a::T, b::T, c::T, d::T, e::T,
+              f::T, g::T, h::T, i::T) where T
+
+    a, b = minmax(a, b)
+    d, e = minmax(d, e)
+    g, h = minmax(g, h)
+
+    b, c = minmax(b, c)
+    e, f = minmax(e, f)
+    h, i = minmax(h, i)
+
+    a, b = minmax(a, b)
+    d, e = minmax(d, e)
+    g, h = minmax(g, h)
+    c, f = minmax(c, f)
+
+    a, d = minmax(a, d)
+    b, e = minmax(b, e)
+    f, i = minmax(f, i)
+
+    d, g = minmax(d, g)
+    e, h = minmax(e, h)
+    c, f = minmax(c, f)
+
+    a, d = minmax(a, d)
+    b, e = minmax(b, e)
+    f, h = minmax(f, h)
+    c, g = minmax(c, g)
+
+    b, d = minmax(b, d)
+    e, g = minmax(e, g)
+
+    c, e = minmax(c, e)
+    f, g = minmax(f, g)
+
+    c, d = minmax(c, d)
+
+    return a, b, c, d, e, f, g, h, i
+end
+
+#    sort 10 values with 29 minmaxs in 9 parallel stages
+
+function sort(a::T,b::T,c::T,d::T,e::T,
+              f::T,g::T,h::T,i::T,j::T) where T
+
+    e, j = minmax(e, j)
+    d, i = minmax(d, i)
+    c, h = minmax(c, h)
+    b, g = minmax(b, g)
+    a, f = minmax(a, f)
+
+    b, e = minmax(b, e)
+    g, j = minmax(g, j)
+    a, d = minmax(a, d)
+    f, i = minmax(f, i)
+
+    a, c = minmax(a, c)
+    d, g = minmax(d, g)
+    h, j = minmax(h, j)
+
+    a, b = minmax(a, b)
+    c, e = minmax(c, e)
+    f, h = minmax(f, h)
+    i, j = minmax(i, j)
+
+    b, c = minmax(b, c)
+    e, g = minmax(e, g)
+    h, i = minmax(h, i)
+    d, f = minmax(d, f)
+
+    c, f = minmax(c, f)
+    g, i = minmax(g, i)
+    b, d = minmax(b, d)
+    e, h = minmax(e, h)
+
+    c, d = minmax(c, d)
+    g, h = minmax(g, h)
+
+    d, e = minmax(d, e)
+    f, g = minmax(f, g)
+
+    e, f = minmax(e, f)
+
+    return a,b,c,d,e,f,g,h,i,j
+end
+
+#    sort 11 values with 35 minmaxs in 9 parallel stages
+
+function sort(a::T, b::T, c::T, d::T, e::T,
+              f::T, g::T, h::T, i::T, j::T,
+              k::T) where T
+
+    a, b = minmax(a, b)
+    c, d = minmax(c, d)
+    e, f = minmax(e, f)
+    g, h = minmax(g, h)
+    i, j = minmax(i, j)
+
+    b, d = minmax(b, d)
+    f, h = minmax(f, h)
+    a, c = minmax(a, c)
+    e, g = minmax(e, g)
+    i, k = minmax(i, k)
+
+    b, c = minmax(b, c)
+    f, g = minmax(f, g)
+    j, k = minmax(j, k)
+    a, e = minmax(a, e)
+    d, h = minmax(d, h)
+
+    b, f = minmax(b, f)
+    g, k = minmax(g, k)
+    e, i = minmax(e, i)
+
+    f, j = minmax(f, j)
+    c, g = minmax(c, g)
+    a, e = minmax(a, e)
+    d, i = minmax(d, i)
+
+    b, f = minmax(b, f)
+    g, k = minmax(g, k)
+    c, d = minmax(c, d)
+    i, j = minmax(i, j)
+
+    b, e = minmax(b, e)
+    h, k = minmax(h, k)
+    d, f = minmax(d, f)
+    g, i = minmax(g, i)
+
+    c, e = minmax(c, e)
+    h, j = minmax(h, j)
+    f, g = minmax(f, g)
+
+    d, e = minmax(d, e)
+    h, i = minmax(h, i)
+
+    return a,b,c,d,e,f,g,h,i,j,k
+end
+
+#    sort 12 values with 39 minmaxs in 9 parallel stages
+
+function sort(a::T,b::T,c::T,d::T,e::T,
+              f::T,g::T,h::T,i::T,j::T,
+              k::T,l::T) where T
+
+    a, b = minmax(a, b)
+    c, d = minmax(c, d)
+    e, f = minmax(e, f)
+    g, h = minmax(g, h)
+    i, j = minmax(i, j)
+    k, l = minmax(k, l)
+
+    b, d = minmax(b, d)
+    f, h = minmax(f, h)
+    j, l = minmax(j, l)
+    a, c = minmax(a, c)
+    e, g = minmax(e, g)
+    i, k = minmax(i, k)
+
+    b, c = minmax(b, c)
+    f, g = minmax(f, g)
+    j, k = minmax(j, k)
+    a, e = minmax(a, e)
+    h, l = minmax(h, l)
+
+    b, f = minmax(b, f)
+    g, k = minmax(g, k)
+    d, h = minmax(d, h)
+    e, i = minmax(e, i)
+
+    f, j = minmax(f, j)
+    c, g = minmax(c, g)
+    a, e = minmax(a, e)
+    h, l = minmax(h, l)
+    d, i = minmax(d, i)
+
+    b, f = minmax(b, f)
+    g, k = minmax(g, k)
+    c, d = minmax(c, d)
+    i, j = minmax(i, j)
+
+    b, e = minmax(b, e)
+    h, k = minmax(h, k)
+    d, f = minmax(d, f)
+    g, i = minmax(g, i)
+
+    c, e = minmax(c, e)
+    h, j = minmax(h, j)
+    f, g = minmax(f, g)
+
+    d, e = minmax(d, e)
+    h, i = minmax(h, i)
+
+    return a,b,c,d,e,f,g,h,i,j,k,l
+end
+
+#    sort 13 values with 45 minmaxs in 10 parallel stages
+
+function sort(a::T, b::T, c::T, d::T, e::T,
+              f::T, g::T, h::T, i::T, j::T,
+              k::T, l::T, m::T) where T
+
+    b, h = minmax(b, h)
+    j, l = minmax(j, l)
+    d, e = minmax(d, e)
+    f, i = minmax(f, i)
+    a, m = minmax(a, m)
+    c, g = minmax(c, g)
+
+    a, b = minmax(a, b)
+    c, d = minmax(c, d)
+    e, g = minmax(e, g)
+    i, l = minmax(i, l)
+    h, m = minmax(h, m)
+    f, j = minmax(f, j)
+
+    a, c = minmax(a, c)
+    d, h = minmax(d, h)
+    k, l = minmax(k, l)
+    b, e = minmax(b, e)
+    g, m = minmax(g, m)
+
+    h, i = minmax(h, i)
+    l, m = minmax(l, m)
+    e, j = minmax(e, j)
+    g, k = minmax(g, k)
+
+    d, e = minmax(d, e)
+    f, g = minmax(f, g)
+    i, j = minmax(i, j)
+    k, l = minmax(k, l)
+    b, h = minmax(b, h)
+
+    c, g = minmax(c, g)
+    j, l = minmax(j, l)
+    b, d = minmax(b, d)
+    e, h = minmax(e, h)
+    i, k = minmax(i, k)
+    a, f = minmax(a, f)
+
+    c, f = minmax(c, f)
+    g, i = minmax(g, i)
+    j, k = minmax(j, k)
+
+    b, c = minmax(b, c)
+    d, f = minmax(d, f)
+    h, i = minmax(h, i)
+    e, g = minmax(e, g)
+
+    c, d = minmax(c, d)
+    e, f = minmax(e, f)
+    g, h = minmax(g, h)
+    i, j = minmax(i, j)
+
+    d, e = minmax(d, e)
+    f, g = minmax(f, g)
+
+    return a,b,c,d,e,f,g,h,i,j,k,l,m
+end
+
+#    sort 14 values with 51 minmaxs in 10 parallel stages
+
+function sort(a::T, b::T, c::T, d::T, e::T,
+              f::T, g::T, h::T, i::T, j::T,
+              k::T, l::T, m::T, n::T) where T
+
+    a, b = minmax(a, b)
+    c, d = minmax(c, d)
+    e, f = minmax(e, f)
+    g, h = minmax(g, h)
+    i, j = minmax(i, j)
+    k, l = minmax(k, l)
+    m, n = minmax(m, n)
+
+    a, c = minmax(a, c)
+    e, g = minmax(e, g)
+    i, k = minmax(i, k)
+    b, d = minmax(b, d)
+    f, h = minmax(f, h)
+    j, l = minmax(j, l)
+
+    a, e = minmax(a, e)
+    i, m = minmax(i, m)
+    b, f = minmax(b, f)
+    j, n = minmax(j, n)
+    c, g = minmax(c, g)
+    d, h = minmax(d, h)
+
+    a, i = minmax(a, i)
+    b, j = minmax(b, j)
+    c, k = minmax(c, k)
+    d, l = minmax(d, l)
+    e, m = minmax(e, m)
+    f, n = minmax(f, n)
+
+    f, k = minmax(f, k)
+    g, j = minmax(g, j)
+    d, m = minmax(d, m)
+    h, l = minmax(h, l)
+    b, c = minmax(b, c)
+    e, i = minmax(e, i)
+
+    b, e = minmax(b, e)
+    h, n = minmax(h, n)
+    c, i = minmax(c, i)
+    f, g = minmax(f, g)
+    j, k = minmax(j, k)
+
+    c, e = minmax(c, e)
+    l, n = minmax(l, n)
+    d, i = minmax(d, i)
+    h, m = minmax(h, m)
+
+    g, i = minmax(g, i)
+    k, m = minmax(k, m)
+    d, f = minmax(d, f)
+    h, j = minmax(h, j)
+
+    d, e = minmax(d, e)
+    f, g = minmax(f, g)
+    h, i = minmax(h, i)
+    j, k = minmax(j, k)
+    l, m = minmax(l, m)
+
+    g, h = minmax(g, h)
+    i, j = minmax(i, j)
+
+    return a,b,c,d,e,f,g,h,i,j,k,l,m,n
+end
+
+#    sort 15 values with 56 minmaxs in 10 parallel stages
+
+function sort(a::T, b::T, c::T, d::T, e::T,
+              f::T, g::T, h::T, i::T, j::T,
+              k::T, l::T, m::T, n::T, o::T) where T
+
+    a, b = minmax(a, b)
+    c, d = minmax(c, d)
+    e, f = minmax(e, f)
+    g, h = minmax(g, h)
+    i, j = minmax(i, j)
+    k, l = minmax(k, l)
+    m, n = minmax(m, n)
+
+    a, c = minmax(a, c)
+    e, g = minmax(e, g)
+    i, k = minmax(i, k)
+    m, o = minmax(m, o)
+    b, d = minmax(b, d)
+    f, h = minmax(f, h)
+    j, l = minmax(j, l)
+
+    a, e = minmax(a, e)
+    i, m = minmax(i, m)
+    b, f = minmax(b, f)
+    j, n = minmax(j, n)
+    c, g = minmax(c, g)
+    k, o = minmax(k, o)
+    d, h = minmax(d, h)
+
+    a, i = minmax(a, i)
+    b, j = minmax(b, j)
+    c, k = minmax(c, k)
+    d, l = minmax(d, l)
+    e, m = minmax(e, m)
+    f, n = minmax(f, n)
+    g, o = minmax(g, o)
+
+    f, k = minmax(f, k)
+    g, j = minmax(g, j)
+    d, m = minmax(d, m)
+    n, o = minmax(n, o)
+    h, l = minmax(h, l)
+    b, c = minmax(b, c)
+    e, i = minmax(e, i)
+
+    b, e = minmax(b, e)
+    h, n = minmax(h, n)
+    c, i = minmax(c, i)
+    l, o = minmax(l, o)
+    f, g = minmax(f, g)
+    j, k = minmax(j, k)
+
+    c, e = minmax(c, e)
+    l, n = minmax(l, n)
+    d, i = minmax(d, i)
+    h, m = minmax(h, m)
+
+    g, i = minmax(g, i)
+    k, m = minmax(k, m)
+    d, f = minmax(d, f)
+    h, j = minmax(h, j)
+
+    d, e = minmax(d, e)
+    f, g = minmax(f, g)
+    h, i = minmax(h, i)
+    j, k = minmax(j, k)
+    l, m = minmax(l, m)
+
+    g, h = minmax(g, h)
+    i, j = minmax(i, j)
+
+    return a,b,c,d,e,f,g,h,i,j,k,l,m,n,o
+end
+
+#    sort 16 values with 60 minmaxs in 10 parallel stages
+
+function sort(a::T, b::T, c::T, d::T, e::T,
+              f::T, g::T, h::T, i::T, j::T,
+              k::T, l::T, m::T, n::T, o::T,
+              p::T) where T
+
+    a, b = minmax(a, b)
+    c, d = minmax(c, d)
+    e, f = minmax(e, f)
+    g, h = minmax(g, h)
+    i, j = minmax(i, j)
+    k, l = minmax(k, l)
+    m, n = minmax(m, n)
+    o, p = minmax(o, p)
+
+    a, c = minmax(a, c)
+    e, g = minmax(e, g)
+    i, k = minmax(i, k)
+    m, o = minmax(m, o)
+    b, d = minmax(b, d)
+    f, h = minmax(f, h)
+    j, l = minmax(j, l)
+    n, p = minmax(n, p)
+
+    a, e = minmax(a, e)
+    i, m = minmax(i, m)
+    b, f = minmax(b, f)
+    j, n = minmax(j, n)
+    c, g = minmax(c, g)
+    k, o = minmax(k, o)
+    d, h = minmax(d, h)
+    l, p = minmax(l, p)
+
+    a, i = minmax(a, i)
+    b, j = minmax(b, j)
+    c, k = minmax(c, k)
+    d, l = minmax(d, l)
+    e, m = minmax(e, m)
+    f, n = minmax(f, n)
+    g, o = minmax(g, o)
+    h, p = minmax(h, p)
+
+    f, k = minmax(f, k)
+    g, j = minmax(g, j)
+    d, m = minmax(d, m)
+    n, o = minmax(n, o)
+    h, l = minmax(h, l)
+    b, c = minmax(b, c)
+    e, i = minmax(e, i)
+
+    b, e = minmax(b, e)
+    h, n = minmax(h, n)
+    c, i = minmax(c, i)
+    l, o = minmax(l, o)
+    f, g = minmax(f, g)
+    j, k = minmax(j, k)
+
+    c, e = minmax(c, e)
+    l, n = minmax(l, n)
+    d, i = minmax(d, i)
+    h, m = minmax(h, m)
+
+    g, i = minmax(g, i)
+    k, m = minmax(k, m)
+    d, f = minmax(d, f)
+    h, j = minmax(h, j)
+
+    d, e = minmax(d, e)
+    f, g = minmax(f, g)
+    h, i = minmax(h, i)
+    j, k = minmax(j, k)
+    l, m = minmax(l, m)
+
+    g, h = minmax(g, h)
+    i, j = minmax(i, j)
+
+    return a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p
+end
+
+
+
+#=
+     sort given a tuple of the values
+=#
+
+for N in collect(NARGS_MIN:NARGS_MAX)
+    @eval (sort(x::NTuple{$N, T}) where T) = sort(x...)
+end
+
+sort(x::Tuple) = ( sort( [x...] )... )
+
+#=
+     sort given a vector of the values
+=#
+
+function sort(v::Vector{T}, xs...) where T
+    n = length(v)
+
+    return
+        if ARGCOUNT_MAX < n
+            sort(v, xs...)
+        elseif n > 0
+            [sort(v...)]
+        else
+            v
+        end
+end
+
+
+end # module SortingNetworks
