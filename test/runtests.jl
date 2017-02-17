a95f1d3aa09994cb13370ca83c684afbf78aff43
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-02-17 17:15:43 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,6 @@ using Base.Test
 for i in 1:25
     tup = ( collect(1:i)... )
     tst = reverse( tup )
-    @test sort(tst) == tup
-    @test sort(tst...) == tup
+    @test swapsort(tst) == tup
+    @test swapsort(tst...) == tup
 end    
