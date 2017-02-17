829089cb12ea142f81bd327e52e98e41b36e83fc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-02-17 17:16:32 -0500

    Update SortingNetworks.jl

--- src/SortingNetworks.jl
+++ src/SortingNetworks.jl
@@ -8,13 +8,13 @@ __precompile__()
     Functions are a sequence of internally parallelizable blocks.
     Functions are written using empty lines as block delmiters.
 
-    `sort(x1::T, .., xn::T)::NTuple{n,T}`  
-    `sort(tup::NTuple{n,T})::NTuple{n,T}`  
+    `swapsort(x1::T, .., xn::T)::NTuple{n,T}`  
+    `swapsort(tup::NTuple{n,T})::NTuple{n,T}`  
 """
 module SortingNetworks
 
-import Base:sort
+export swapsort
 
-include("sort.jl")
+include("swapsort.jl")
 
 end # module
