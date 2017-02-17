5986e0504c4244aa33d2c458d66d63dbb35f6121
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-02-17 08:47:18 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using SortingNetworks
 using Base.Test
 
 for i in 1:25
-    tup = ( collect((1:i)... )
+    tup = ( collect(1:i)... )
     tst = reverse( tup )
     @test sort(tst) == tup
     @test sort(tst...) == tup
