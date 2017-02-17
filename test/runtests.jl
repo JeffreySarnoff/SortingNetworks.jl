5090f64aeb260c6838e7698d6d51f3643ad5aed1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-02-17 08:44:22 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using SortingNetworks
 using Base.Test
 
-@test sort(5,4,3,2,1) == (1,2,3,4,5)
-@test sort(10,9,8,7,6,5,4,3,2,1) == (1,2,3,4,5,6,7,8,9,10)
-
-@test sort( (3,2,1) ) == (1,2,3)
+for i in 1:25
+    tup = ( collect((1:i)... )
+    tst = reverse( tup )
+    @test sort(tst) == tup
+    @test sort(tst...) == tup
+end    
