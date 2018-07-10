0c007fe0640fb965912d3d9c7eeb8af025edc596
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-10 08:13:41 -0400

    Update swapsort_extras.jl

--- src/swapsort_extras.jl
+++ src/swapsort_extras.jl
@@ -1,4 +1,4 @@
-swapsort(x::NTuple{N, T}) where {N, T} = swapsort(x...,)
+@inline swapsort(x::NTuple{N, T}) where {N, T} = swapsort(x...,)
 
-swapsort(x::V) where {T, V<:AbstractVector{T}} = swapsort(x...,)
+@inline swapsort(x::V) where {T, V<:AbstractVector{T}} = swapsort(x...,)
 
