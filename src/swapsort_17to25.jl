747ff063c3da2bc3bf4fd2bda0d01b1008f1c92a
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2024-02-19 18:33:02 +0000

    remove a repeated method

--- src/swapsort_17to25.jl
+++ src/swapsort_17to25.jl
@@ -130,7 +130,6 @@ function swapsort(a::T, b::T, c::T, d::T, e::T,
     return a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q
 end
 
-@inline swapsort(x::NTuple{17,T}) where {T} = swapsort(x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12], x[13], x[14], x[15], x[16], x[17])
 @inline swapsort(::Type{Val{17}}, x::AbstractVector{T}) where {T} = swapsort(x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12], x[13], x[14], x[15], x[16], x[17])
 
 #    sort 18 values with 82 minmaxs in 13 parallel stages
